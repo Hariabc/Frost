538763e1cdc1e941085bb52190225e2c6c8ad27b
Hariabc <vorugantihariprasad97@gmail.com>
2024-01-20 16:35:55 +0530

    ....

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
-import './App.css'
-import LoginPage from './pages/LoginPage'
+import React from 'react';
+import './App.css';
+import LoginPage from './pages/LoginPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import StudentDashboard from './pages/StudentDashboard';
+import TeacherDashboard from './pages/TeacherDashboard';
 
 function App() {
   return (
-    <>
-      <LoginPage/>
-    </>
-  )
+    <Router>
+      <Routes>
+      <Route index element={<LoginPage/>}/>
+         <Route path='/student' element={<StudentDashboard/>}/>
+         <Route path='/teacher' element={<TeacherDashboard/>}/>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/TeacherDashboard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TeacherDashboard() {
+  return (  
+      <h1>Teacher</h1> 
+)
+}
