b8e4b675bfcf51a96b4211f3203d5766f1742cd3
Hariabc <vorugantihariprasad97@gmail.com>
2024-01-20 17:19:41 +0530

    ...

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -3,7 +3,7 @@ import { App as SendbirdApp } from "sendbird-uikit";
 import "sendbird-uikit/dist/index.css";
 import './chat.css'
 export default function Chat() {  
-    userId="Hari"  
+    const userId="Hari"  
     const APP_ID = "EAD127B3-C2FC-47A7-B744-D1F2DE076DB5";   
 return (
     <div className="App" style={{
